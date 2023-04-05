ac22a279db34850740a68cc2626d5c0398cf9eea
Manvir Kaur Brar <w0805615@myscc.ca>
2023-04-05 19:33:45 -0400

    Modified startup becuase there was errror in using statement. Moved Models in ManvirBooks.Models

--- ManvirBookStore/Startup.cs
+++ ManvirBookStore/Startup.cs
@@ -1,4 +1,5 @@
-using ManvirBookStore.Data;
+//using ManvirBookStore.Data;
+using ManvirBookStore.DataAccess.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
