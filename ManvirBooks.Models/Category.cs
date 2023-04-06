f648009ce4ab7b47f4bf778d08f3d6177b5c1e03
Manvir Kaur Brar <w0805615@myscc.ca>
2023-04-05 20:15:57 -0400

    added class in models "Category.cs". added migration. updated database. Checked the tables in SQl Server ObjectExplorer

---
+++ ManvirBooks.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ManvirBooks.Models
+{
+    public class Category
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Display(Name = "Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+
+    }
+}
