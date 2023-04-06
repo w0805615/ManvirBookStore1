b990b51cd1214296ed24e6b508ed3c934569f54c
Manvir Kaur Brar <w0805615@myscc.ca>
2023-04-05 20:33:57 -0400

    Added new interface called ISP_Call.cs and modified as given instructions. And Added new class "SP_Call.cs"

--- ManvirBooks.DataAccess/Repository/IRepository/ISP_Call.cs
+++ ManvirBooks.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ManvirBooks.DataAccess.Repository.IRepository
 {
-    interface ISP_Call
+    public interface ISP_Call : IDisposable
     {
+        T Single<T>(string procedurename, DynamicParameters param = null);
+        void Execute(string procedurename, DynamicParameters param = null);
+        T OneRecord<T>(string procedurename, DynamicParameters param = null);
+        IEnumerable<T> List<T>(string procedurename, DynamicParameters param = null);
+
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedurename, DynamicParameters param = null);
     }
 }
