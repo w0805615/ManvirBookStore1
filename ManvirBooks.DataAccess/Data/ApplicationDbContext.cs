a9e1fcb382e6c8201b0fe72d60ace1a99f5d2e5c
Manvir Kaur Brar <W0805615@myscc.ca>
2023-04-13 22:04:23 -0400

    Solving Error

--- ManvirBooks.DataAccess/Data/ApplicationDbContext.cs
+++ ManvirBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace ManvirBookStore.DataAccess.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
-        public object Covers { get; internal set; }
+        public DbSet<CoverType> CoverTypes { get; set; }
+    
     }
 }
