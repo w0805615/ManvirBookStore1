399f1f7f8cbaf6292994957b41c1e1ea1f0f83da
Manvir Kaur Brar <W0805615@myscc.ca>
2023-04-19 11:53:02 -0400

    Product Corrections. product.js added delte method. Upsert.cs added Covertype list to show listitems in product page.

--- ManvirBookStore/wwwroot/js/product.js
+++ ManvirBookStore/wwwroot/js/product.js
@@ -20,11 +20,11 @@ function loadDataTable() {
                 "data": "id",
                 "render": function (data) {
                     return `
-                            <div class="text-center">
+                           <div class="text-center">
                                 <a href="/Admin/Product/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
                                     <i class="fas fa-edit"></i>&nbsp;
                                 </a>
-                                <a class="btn btn-danger text-white" style="cursor:pointer">
+                                <a onclick=Delete("/Admin/Product/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
                                     <i class="fas fa-trash-alt"></i>&nbsp;
                                 </a>
                             </div>
@@ -33,4 +33,30 @@ function loadDataTable() {
             }
         ]
     });
+}
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the data!",
+        icon: "Warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    })
 }
